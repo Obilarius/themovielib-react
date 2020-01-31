f921f7afaefd6b9c2e15d504a94087e2e9ca4f4c
Obilarius <git@walzenbach.com>
2020-01-31 14:37:56 +0100

    delete all

--- src/index.js
+++ src/index.js
@@ -1,17 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { CookiesProvider } from "react-cookie";
 import App from "./components/app/App";
 import * as serviceWorker from "./serviceWorker";
 import "./fontawesome";
 import "./CSS/index.scss";
 
-ReactDOM.render(
-  <CookiesProvider>
-    <App />
-  </CookiesProvider>,
-  document.getElementById("root")
-);
+ReactDOM.render(<App />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
