84d5435ab32812bf4813699da52d65a5a964bd65
Obilarius <git@walzenbach.com>
2020-03-02 11:07:51 +0100

    Navbar fixed Position

--- src/components/shared/Navbar/Navbar.jsx
+++ src/components/shared/Navbar/Navbar.jsx
@@ -6,34 +6,36 @@ import "./Navbar.scss";
 
 const Navbar = () => {
   return (
-    <nav id="navbar">
-      <div className="container">
-        <Link className="navbar__brand" to="/">
-          <FontAwesomeIcon icon={["fad", "film"]} />
-          TMLIB
-        </Link>
-        <div className="navbar__linklist">
-          <NavLink className="nav-link" to="/watchlist">
-            Watchlist
-          </NavLink>
-          <NavLink className="nav-link" to="/diary">
-            Diary
-          </NavLink>
-          <NavLink className="nav-link" to="/library">
-            Library
-          </NavLink>
-          <NavLink className="avatar" to="/user">
-            <div />
-          </NavLink>
-          {/* <NavLink className="nav-link" to="/login">
+    <div id="navbar__wrapper">
+      <nav id="navbar">
+        <div className="container">
+          <Link className="navbar__brand" to="/">
+            <FontAwesomeIcon icon={["fad", "film"]} />
+            TMLIB
+          </Link>
+          <div className="navbar__linklist">
+            <NavLink className="nav-link" to="/watchlist">
+              Watchlist
+            </NavLink>
+            <NavLink className="nav-link" to="/diary">
+              Diary
+            </NavLink>
+            <NavLink className="nav-link" to="/library">
+              Library
+            </NavLink>
+            <NavLink className="avatar" to="/user">
+              <div />
+            </NavLink>
+            {/* <NavLink className="nav-link" to="/login">
           Login
         </NavLink>
         <NavLink className="nav-link" to="/register">
           Register
         </NavLink> */}
+          </div>
         </div>
-      </div>
-    </nav>
+      </nav>
+    </div>
   );
 };
 
